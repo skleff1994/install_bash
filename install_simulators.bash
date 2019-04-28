4898402ec05a75cb4393d223d330cf02f4f8e9a4
Avadesh Meduri <ameduri@fuji.is.localnet>
2019-04-28 19:35:18 +0200

    updated bash to install gym

--- install_simulators.bash
+++ install_simulators.bash
@@ -1,21 +1,11 @@
-## This bash file is to install different simulators
+## This bash file is to install pybullet and gym
 
 ## installtion for pybullet
 sudo -H pip install pybullet
 
-## installtion for dartsim
 
-# Add the Dartsim ppa
-add-apt-repository -y ppa:dartsim/ppa
-
-# update apt package list
-sudo apt-get update
-
-# install Dartsim
-# Ubuntu14.04: libdart5-dev
-# Ubuntu16.04: libdart6-dev
-sudo apt-get install libdart6-dev
-
+### installation of open AI gym
+sudo -H pip install gym
 
 
 
