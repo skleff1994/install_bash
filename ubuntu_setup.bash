2eb7eaf877eb51bf12fa762a471ce7f9ca828504
Avadesh Meduri <avadesh02@gmail.com>
2019-05-22 22:39:43 +0530

    updated ubunut install for spotify

--- ubuntu_setup.bash
+++ ubuntu_setup.bash
@@ -241,3 +241,11 @@ sudo apt-get install paper-icon-theme paper-gtk-theme
 
 ### install desktop image ################################################################
 sudo apt-get install ubuntu-wallpapers* 
+
+######## installing spotify ###################################
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+source ~/.basrc
